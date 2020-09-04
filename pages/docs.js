1fa7263d510bfea10c3a93be981b46f2d2a76c33
Robert Harkov <robertharkov@gmail.com>
2020-09-03 22:26:25 -0400

    added links and ability to switch md view on the page

--- pages/docs.js
+++ pages/docs.js
@@ -1,9 +1,15 @@
 import styles from "../styles/Home.module.css";
+const { useState } = require("react");
 import { Button } from "@material-ui/core";
 import Test from "./test.mdx";
-import Home from "./home.mdx";
+import Introduction from "./introduction.mdx";
+import Installation from "./installation.mdx";
+import Functionality from "./functionality.mdx";
 
 export default function Docs() {
+  const [currentMd, setCurrentMd] = useState("introduction");
+  const [activeLink, setActiveLink] = useState(false);
+
   return (
     <div className={styles.doc__container}>
       <div className={styles.doc__navLinks}>
@@ -11,11 +17,20 @@ export default function Docs() {
         <h2 className={styles.doc__smallTitle}>Documentation</h2>
         <div className={styles.doc__flexContainer}>
           <div className={styles.doc__docLinks}>
-            <a href="#">HELLO</a>
+            <a onClick={() => setCurrentMd("introduction")}>Introduction</a>
+            <br />
+            <br />
+            <a onClick={() => setCurrentMd("installation")}>
+              Installation & Usage
+            </a>
+            <br />
+            <br />
+            <a onClick={() => setCurrentMd("functionality")}>Functionality</a>
           </div>
           <div className={styles.doc__testing}>
-            <Home></Home>
-            {/* <Test></Test> */}
+            {currentMd === "introduction" && <Introduction></Introduction>}
+            {currentMd === "installation" && <Installation></Installation>}
+            {currentMd === "functionality" && <Functionality></Functionality>}
           </div>
         </div>
       </div>
