96e5299174d9f4f9b5623339841db33d6504c75d
Robert Harkov <robertharkov@gmail.com>
2020-09-08 12:43:27 -0400

    changed the logo

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,12 +1,11 @@
 import styles from "../../styles/Home.module.css";
 
 export default function Header() {
-
   return (
     <div className={styles.header}>
-      <a href="/" >
-        <img src="/GGLogo.png" style={{width: "12vw"}}/>
+      <a href="/">
+        <img src="/GGLogo-v2.png" style={{ width: "12vw" }} />
       </a>
     </div>
-  )
+  );
 }
