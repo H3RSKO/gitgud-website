705c80dffa9b15fd7aabcb93f419a8a027d08f61
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-09-09 15:04:34 -0400

    added aboutus outline

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -1,9 +1,16 @@
 import styles from "../styles/Home.module.css";
+import Header from "./components/header";
 
 export default function Docs() {
   return (
     <div className={styles.doc__container}>
-      <h2 className={styles.doc__smallTitle}>About us!</h2>
+      <Header />
+      <h2 >About us!</h2>
+      <div>
+          <h2>Robert Harkov</h2>
+          <a href="https://www.linkedin.com/in/robert-harkov/" >LinkeIn</a>
+          <a href="https://github.com/rharkov" target="_blank" rel="noopener noreferrer">github</a>
+      </div>
     </div>
   );
 }
